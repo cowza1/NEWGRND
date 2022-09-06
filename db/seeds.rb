c48709e7a41086bf143bdacdcebe49e897477fcb
Toby Higgins <higginstobias2@gmail.com>
2022-09-06 16:31:53 +0100

    done the design seeds some of them

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,124 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "deleting designers"
+Designer.destroy_all
+puts "creating designers"
+Designer.create!(
+  name: "Albert.",
+  image: "https://cdn.shopify.com/s/files/1/0582/5921/0426/files/ALBERT_LOGO_320x.png?v=1644865712",
+  description: "Founded in 2008, each Albert tailored piece is made with heavy influence from 1940’s British style.
+                From the design stage to the final piece we leave no stone unturned,
+                ensuring the most classic yet stylish pieces you can find.",
+  social: "https://www.instagram.com/albertclothing/?hl=en",
+  url: "https://albertclothing.com/",
+  style: "preppy",
+  budget: "high",
+  colour_palette: "monochrome",
+  gender: "men"
+)
+Designer.create!(
+  name: "Cooba",
+  image: "https://cdn.shopify.com/s/files/1/0278/7452/5262/files/SHOPIFY_LOGO_400x.png?v=1573873988",
+  description: "Cooba is manufactured and distributed from London, born from a vision to produce timeless capsules
+                through sourcing the finest fabrics within the capital.
+                incorporating both ready-to-wear and fully bespoke pieces, this is the answer to affordable luxury.",
+  social: "https://www.instagram.com/coobalondon/",
+  url: "https://www.coobalondon.co.uk/",
+  style: "casual",
+  budget: "mid",
+  colour_palette: "monochrome",
+  gender: "men"
+)
+Designer.create!(
+  name: "Cole Buxton",
+  image: "https://cdn.shopify.com/s/files/1/0055/1023/4230/files/Logo.png?v=1614321543",
+  description: " The Cole Buxton brand is a collection of daily essentials. the concept started in 2014 with the idea to
+                 redefine contemporary sportswear for the modern mans lifestyle. with the aim to 'master simplicity'
+                 through our design value of 'athletic essentialism' we are continuously refining a
+                 core garment collection intended for everyday use.",
+  social: "https://www.instagram.com/cole_buxton/?hl=en",
+  url: "https://www.colebuxton.com/?gclid=Cj0KCQjw39uYBhCLARIsAD_SzMSCRF8kwqBWt96Hv_elcLbKmpaP8wzRxsYVIfmm3QTEsKMCWQVrsjYaAvCWEALw_wcB",
+  style: "casual",
+  budget: "medium",
+  colour_palette: "neutral",
+  gender: "men"
+)
+Designer.create!(
+  name: "BALR.",
+  image: "https://upload.wikimedia.org/wikipedia/commons/3/37/BALR._Logo.png",
+  description: "The brand is known for designs with the colours black and white.
+                BALR. aims to make the life of a professional football player accessible to a wider audience",
+  social: "https://www.instagram.com/balr/?hl=en",
+  url: "https://www.balr.com/?gclid=Cj0KCQjw39uYBhCLARIsAD_SzMSTd-9mszjDu79kSdb4GPvh-3iCXw0y22CyZQ8D7L0rOIEf7_bXHeUaAqh3EALw_wcB",
+  style: "casual",
+  budget: "medium",
+  colour_palette: "monochrome",
+  gender: "men"
+)
+Designer.create!(
+  name: "PREVU",
+  image: "https://cdn.shopify.com/s/files/1/0685/9051/files/PrevuLogo_Alternative_Black_b85895bb-361a-41c4-b31c-bd8bb6418ee0_1000x1000.png?v=1659621761",
+  description: "Created in 2015, Prévu is a contemporary menswear design house based in the heart of East London - designed by Jake Hall.
+                Prévu is a contemporary design studio in East London. Specialising in modern designer clothing, creative is led by Jake Hall.
+                Collections include twinsets, tracksuits, jackets, coats, T-shirts,
+                hoodies, polos, trousers and shorts, alongside accessories including hats and fragrances.",
+  social: "https://www.instagram.com/prevustudio/?hl=en",
+  url: "https://www.prevustudio.com/",
+  style: "preppy",
+  budget: "medium",
+  colour_palette: "monochrome",
+  gender: "men"
+)
+Designer.create!(
+  name: "Daniel Diyepriye",
+  image: "https://www.danieldiyepriye.com/wp-content/uploads/2020/05/Daniel-Diyepriye-white-smaller.png",
+  description: "Daniel Diyepriye is a luxury African heritage fashion house and brand.
+                Envisioned by Tubonimi Nkari Dokubo, who sadly passed away in April of 2020.",
+  social: "https://www.instagram.com/danieldiyepriye/?hl=en",
+  url: "https://www.danieldiyepriye.com/",
+  style: "cas",
+  budget: "high",
+  colour_palette: "monochrome",
+  gender: "men"
+)
+Designer.create!(
+  name: "Daily Paper",
+  image: "https://cdn.shopify.com/s/files/1/0067/3592/1221/files/logo-2---black.png?v=13542309051390421382",
+  description: "Daily Paper is an Amsterdam-based fashion and lifestyle brand established in 2012 by three childhood friends.
+                The brand is fueled by the rich heritage
+                of African culture wrapped in contemporary designs,
+                The once blog established by Hussein, Jefferson and Abderrahmane shared a love for music, art, fashion, and culture,
+                growing the brand into a household name.",
+  social: "https://www.instagram.com/dailypaper/?hl=en",
+  url: "https://us.dailypaperclothing.com/",
+  style: "casual",
+  budget: "medium",
+  colour_palette: "pastel",
+  gender: "men"
+)
+Designer.create!(
+  name: "Private",
+  image: "https://cdn.shopify.com/s/files/1/0310/5426/6500/files/checkout_6b1c4e71-83f4-4593-a7aa-dc494d3ac20f.jpg?height=628&pad_color=fff&v=1592679844&width=1200",
+  description: "A Private Co. is a European-based fashion and lifestyle brand serving the youth since 2016." ,
+  social: "https://www.instagram.com/aprivateco/?hl=en",
+  url: "https://aprivate.co/",
+  style: "casual",
+  budget: "medium",
+  colour_palette: "pastel",
+  gender: "men"
+)
+Designer.create!(
+  name: "Bode",
+  image: "https://www.vooberlin.com/media/image/ee/6c/06/bode_logo_00_cbf3328c-c2e9-4b16-8605-4d8c24782cef_180x-2x.png",
+  description: "Bode is a luxury menswear brand that expresses a sentimentality
+                for the past through the study of personal narratives and historical techniques.
+                Modern workwear silhouettes united with female-centric traditions of quilting, mending,
+                and appliqué shape the collections. each piece tells a story and is tailor-made.",
+  social: "https://www.instagram.com/bode/?hl=en",
+  url: "https://www.bodenewyork.com/",
+  style: "casual",
+  budget: "high",
+  colour_palette: "pastel",
+  gender: "men"
+)
+
+
+puts "finished"
