c70f48a208720cdc7a72ed48ad0ee6083aa23ed1
adam couser <couser1304@gmail.com>
2022-09-07 17:25:15 +0100

    results page

--- db/schema.rb
+++ db/schema.rb
@@ -79,7 +79,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_155134) do
     t.string "name"
     t.text "description"
     t.string "image"
-    t.string "price"
+    t.integer "price"
     t.bigint "designer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -102,9 +102,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_155134) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "email"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
