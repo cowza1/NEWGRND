f2d32e98f58c51496c42c32abcb1d8cb9d60f65e
adam couser <couser1304@gmail.com>
2022-09-08 14:01:50 +0100

    designer item show page

--- app/controllers/designers_controller.rb
+++ app/controllers/designers_controller.rb
@@ -1,2 +1,10 @@
 class DesignersController < ApplicationController
+  def index
+    @designers = Designer.all
+  end
+
+  def show
+    @designer = Designer.find(params[:id])
+    @products = Product.where(params[:designer_id])
+  end
 end
