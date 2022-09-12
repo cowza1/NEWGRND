d1469dab7060aed9c5aad38cb56e8e3c84e8bbda
adam couser <couser1304@gmail.com>
2022-09-12 12:51:11 +0100

    cart function

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module NEWGRND
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # config.eager_load_paths << Rails.root.join("app/services")
+    config.autoload_once_paths << "#{root}/app/services"
   end
 end
