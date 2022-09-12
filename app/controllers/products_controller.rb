824a2fac816098b5743cc7e7884310fe4cd81921
adam couser <couser1304@gmail.com>
2022-09-12 13:03:32 +0100

    ]cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,8 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @designer = Product.find(@product.designer_id)
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+  end
 end
